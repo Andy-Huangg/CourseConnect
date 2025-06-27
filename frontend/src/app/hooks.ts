2721721b344d31594769b53b71bbf233bf35232a
Andy Huang <ahua579@aucklanduni.ac.nz>
2025-06-27 14:32:11 +1200

    Update to build frontend correctly

--- frontend/src/app/hooks.ts
+++ frontend/src/app/hooks.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
-import type { RootState, AppDispatch } from "./index";
+import type { RootState, AppDispatch } from "./store";
 
 // Use these throughout your app instead of plain useDispatch/useSelector
 export const useAppDispatch = () => useDispatch<AppDispatch>();
